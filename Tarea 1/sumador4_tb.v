e3589a77891c343a7e4d1850cc316a5594cc5c41
Kataku69 <kataku26@gmail.com>
2023-03-27 17:03:20 -0600

    Pruebas #1-#4 del testbench

--- Tarea 1/sumador4_tb.v
+++ Tarea 1/sumador4_tb.v
@@ -32,26 +32,62 @@ module sumador4_tb;
     initial begin
         $dumpfile("sumador4_tb.vcd");
         $dumpvars(0, sumador4_tb);
-        CLK = 0;
-        ENB = 1;
-        MODO = 2'b10;
+
+        
         RCI = 0;
+
+        // PRUEBA #1 SUMA DE 4 BITS
+        MODO = 2'b11;
+        ENB = 1;
+        CLK = 0;
+
+        #10;
+        MODO = 2'b01;
         A = 4;
         B = 3;
 
         #10;
+        A = 4'b1111;
+        B = 1'b1;
+
+        #10;
         A = 7;
         B = 2;
-        MODO = 2'b00;
+
+        #10;
+        // PRUEBA #2 RESTA DE 4 BITS
+        MODO = 2'b11; 
+
+        #10;
+        MODO = 2'b10;
+        A = 4;
+        B = 3;
 
         #10;
         A = 4'b1111;
         B = 1'b1;
-        MODO = 2'b11;
 
         #10;
-        A = 0;
-        B = 0;
+        A = 2;
+        B = 8;
+
+        #10;
+        // PRUEBA #3 MANTENER EL VALOR EN MODO 00 
+        MODO = 2'b01; // OPERACION INICIAL
+        A = 5;
+        B = 3;
+        
+        #10;
+        MODO = 2'b00; // MANTENER RESULTADO DE SUMA
+
+        #10;
+        // PRUEBA #4 MANTENER EL VALOR CUANDO ENB = 0
+        MODO = 2'b10; // OPERACION INICIAL
+        A = 3;
+        B = 1;
+
+        #10;
+        ENB = 0;
 
         #10;
         $finish;
