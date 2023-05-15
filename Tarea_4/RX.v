7204ea0fe907113920dac8192b068af042d8a752
Kataku69 <kataku26@gmail.com>
2023-05-15 10:25:31 -0600

    even messier update

--- Tarea_4/RX.v
+++ Tarea_4/RX.v
@@ -1,4 +1,36 @@
 module RX(  input   CKP, CPH, SCK, SS, MOSI,
             output  MISO);
 
+// Registros internos
+reg [1:0]   CONTADOR;
+reg [13:0]  REGISTRO;
+reg [7:0]   BYTE_OUT;
+reg [7:0]   BYTE_IN;
+reg [2:0]   ESTADO;  
+reg [2:0]   PROX_ESTADO;  
+
+// Declaración de estados
+parameter ESPERA    = 3'b001;
+parameter OPERACION = 3'b010;
+parameter FINAL     = 3'b100;
+
+// Descripción de Flip-Flops
+always @(posedge SCK) begin
+    if (!RESET) begin
+        ESTADO <= ESPERA;
+    end else begin
+        ESTADO <= PROX_ESTADO;    
+    end
+end
+
+// Lógica de próximo estado
+always @(*) begin
+    case (ESTADO)
+        ESPERA:     PROX_ESTADO = (!SS) ? OPERACION : ESPERA;
+        OPERACION:  PROX_ESTADO = (!SS) ? OPERACION : FINAL;
+        FINAL:      PROX_ESTADO = ESPERA;
+        default:    PROX_ESTADO = ESPERA;
+    endcase
+    
+end
 endmodule
--- Tarea_4/TX.v
+++ Tarea_4/TX.v
@@ -1,20 +1,50 @@
-module TX(  input   CPK, CPH, CLK, RESET, MISO,
+module TX(  input   CKP, CPH, CLK, RESET, MISO,
             output  SCK, CS, MOSI);
-
-
-// Registro contador
-reg [1:0] CONTADOR;
 
 // Generación del SCK
 always @(posedge CLK) begin
     if (CONTADOR == 2'b11) begin
-        CONTADOR <= 2'b00:
+        CONTADOR <= 2'b00;
     end else begin
         CONTADOR <= CONTADOR + 1;
     end
 end
-
 assign SCK = &CONTADOR;
+
+// Registros internos
+reg [1:0]   CONTADOR;
+reg [13:0]  REGISTRO;
+reg [7:0]   BYTE_OUT;
+reg [7:0]   BYTE_IN;
+reg [2:0]   ESTADO;  
+reg [2:0]   PROX_ESTADO;  
+
+// Declaración de estados
+parameter ESPERA    = 3'b001;
+parameter OPERACION = 3'b010;
+parameter FINAL     = 3'b100;
+
+// Descripción de Flip-Flops
+always @(posedge SCK) begin
+    if (!RESET) begin
+        ESTADO <= ESPERA;
+    end else begin
+        ESTADO <= PROX_ESTADO;    
+    end
+end
+
+// Lógica de próximo estado
+always @(*) begin
+    case (ESTADO)
+        ESPERA: ;
+        OPERACION:  ;
+        FINAL:      ;
+        default: PROX_ESTADO = ESPERA;
+    endcase
+    
+end
+
+
 
 
 endmodule
